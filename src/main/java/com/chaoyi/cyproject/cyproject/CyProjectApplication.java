7bd412e26bdbddb0f3c56ad053eead018a767f2c
suansuan <3241724463@qq.com>
2021-07-08 15:57:31 +0800

    test第一次提交郭锦

---
+++ src/main/java/com/chaoyi/cyproject/cyproject/CyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chaoyi.cyproject.cyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyProjectApplication.class, args);
+    }
+
+}
